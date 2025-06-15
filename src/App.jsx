cc301b20a5e473f7a455a8425f06e9936d89803f
Yash Shinde <yash73shinde@gmail.com>
2025-06-16 00:22:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { } from 'react'
+
+
+
+function App() {
+
+ return (
+  <div>
+  <h1 className="text-3xl font-rubik font-bold underline">
+    Hello World
+  </h1>
+  <h1 className="text-3xl font-savate font-bold underline"> Hello World</h1>
+  <h1 className="text-3xl font-roboto font-bold underline">
+    Hello World
+  </h1></div>
+ )
+
+}
+
+export default App
